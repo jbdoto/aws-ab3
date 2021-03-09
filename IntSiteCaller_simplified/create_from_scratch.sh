9a70a9164aeda71c33d74844f0d1bfaa20511336
Jeffrey Doto <jeffdoto@amazon.com>
2021-03-09 10:10:36 -0500

    Adds waiters to create_from_scratch.sh

--- IntSiteCaller_simplified/create_from_scratch.sh
+++ IntSiteCaller_simplified/create_from_scratch.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 # Templates for docker repos are in their respective projects.
-# aws cloudformation --region=us-east-1 --profile=jdoto-ab3 create-stack --stack-name CfnIntSiteCallerRepository --template-body file://repository.yml --capabilities CAPABILITY_NAMED_IAM  --output text;
-# aws cloudformation create-stack --stack-name IntSiteCallerUploadHandlerECRRepository --template-body file://repository.yml  --profile=jdoto-ab3 --region=us-east-1
-# aws cloudformation create-stack --stack-name IntSiteCallerReportECRRepository --template-body file://repository.yml  --profile=jdoto-ab3 --region=us-east-1
 
-aws cloudformation create-stack --stack-name IntSiteCallerVPC --template-body file://VPC.yml  --profile=jdoto-ab3 --region=us-east-1;
-aws cloudformation create-stack --stack-name IntSiteCallerBuckets --template-body file://BucketConfiguration.yml  --profile=jdoto-ab3 --region=us-east-1 --capabilities CAPABILITY_NAMED_IAM
-aws cloudformation create-stack --stack-name IntSiteCallerBatchConfiguration --template-body file://BatchConfiguration.yml  --profile=jdoto-ab3 --region=us-east-1 --capabilities CAPABILITY_NAMED_IAM
-aws cloudformation create-stack --stack-name IntSiteCallerPipelineDashboard --template-body file://Dashboard.yml  --profile=jdoto-ab3 --region=us-east-1 --capabilities CAPABILITY_NAMED_IAM
+aws cloudformation create-stack --stack-name IntSiteCallerVPC --template-body file://VPC.yml  --profile=jdoto-ab3 --region=us-east-1; aws cloudformation wait stack-update-complete --stack-name IntSiteCallerVPC
+aws cloudformation create-stack --stack-name IntSiteCallerBuckets --template-body file://BucketConfiguration.yml  --profile=jdoto-ab3 --region=us-east-1 --capabilities CAPABILITY_NAMED_IAM CAPABILITY_AUTO_EXPAND CAPABILITY_IAM; aws cloudformation wait stack-update-complete --stack-name IntSiteCallerBuckets
+aws cloudformation create-stack --stack-name IntSiteCallerBatchConfiguration --template-body file://BatchConfiguration.yml  --profile=jdoto-ab3 --region=us-east-1 --capabilities CAPABILITY_NAMED_IAM; aws cloudformation wait stack-update-complete --stack-name IntSiteCallerBatchConfiguration
+aws cloudformation create-stack --stack-name IntSiteCallerPipelineDashboard --template-body file://Dashboard.yml  --profile=jdoto-ab3 --region=us-east-1 --capabilities CAPABILITY_NAMED_IAM; aws cloudformation wait stack-update-complete --stack-name IntSiteCallerPipelineDashboard
 
